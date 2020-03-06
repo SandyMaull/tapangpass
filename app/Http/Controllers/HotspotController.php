cae31be1dc3b6913aba34bb5faa34530448433ac
SandyMaull <sandymaulana0@gmail.com>
2020-03-06 20:50:22 +0800

    ADD CSRF Token for External Captive Portal

--- app/Http/Controllers/HotspotController.php
+++ app/Http/Controllers/HotspotController.php
@@ -8,19 +8,20 @@ class HotspotController extends Controller
 {
     public function login(Request $request)
     {
-        if($request->has('ujank_gaming')) {
-            if($request->ujank_gaming == 'Kuronekosan_Server')
-            {
-                return view('tapangpass/Homepage/login');
-            }
-            else {
-               return redirect('/tapangpass');
-            }
+        // if($request->has('ujank_gaming')) {
+        //     if($request->ujank_gaming == 'Kuronekosan_Server')
+        //     {
+        //         return view('tapangpass/Homepage/login');
+        //     }
+        //     else {
+        //        return redirect('/tapangpass');
+        //     }
             
-        }
-        else {
-           return redirect('/tapangpass');
-        }
+        // }
+        // else {
+        //    return redirect('/tapangpass');
+        // }
+        dd($request->all());
     }
 
     public function csrf(Request $request)
