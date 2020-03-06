c892c41fae65e4f633e93473e14d1d40c71c7f0f
SandyMaull <sandymaulana0@gmail.com>
2020-03-06 18:14:27 +0800

    Add Hotspot External Captive

--- app/Http/Controllers/HotspotController.php
+++ app/Http/Controllers/HotspotController.php
@@ -8,21 +8,21 @@ class HotspotController extends Controller
 {
     public function login(Request $request)
     {
-        // if($request->all() != null) {
-        //     if($request->ujank_gaming == 'Kuronekosan_Server')
-        //     {
-        //         // dd($request->all());
-        //         return view('tapangpass/Homepage/login');
-        //     }
-        //     else {
-        //         redirect('/tapangpass');
-        //     }
+        if($request->has('ujank_gaming')) {
+            if($request->ujank_gaming == 'Kuronekosan_Server')
+            {
+                // dd($request->all());
+                return view('tapangpass/Homepage/login');
+            }
+            else {
+                redirect('/tapangpass');
+            }
             
-        // }
-        // else {
-        //     redirect('/tapangpass');
-        // }
+        }
+        else {
+            redirect('/tapangpass');
+        }
         // dd(Request::exists('ujank_gaming'));
-        dd($request->has('ujank_gaming'));
+        // dd($request->has('ujank_gaming'));
     }
 }
