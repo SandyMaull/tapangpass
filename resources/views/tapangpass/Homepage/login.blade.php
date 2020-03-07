185108392573ecbced9916689fafe89ae9ca20c8
SandyMaull <sandymaulana0@gmail.com>
2020-03-07 10:42:07 +0800

    Test Voucher Template

--- resources/views/tapangpass/Homepage/login.blade.php
+++ resources/views/tapangpass/Homepage/login.blade.php
@@ -2,26 +2,25 @@
 
 @section('HS-Script')
 <?php
-    $mac=$_GET['mac'];
-    $ip=$_GET['ip'];
-    $username=$_GET['username'];
-    $linklogin=$_GET['link-login'];
-    $linkorig=$_GET['link-orig'];
-    $error=$_GET['error'];
-    $trial=$_GET['trial'];
-    // $loginby=$_GET['login-by'];
-    $chapid=$_GET['chap-id'];
-    $chapchallenge=$_GET['chap-challenge'];
-    $linkloginonly=$_GET['link-login-only'];
-    $linkorigesc=$_GET['link-orig-esc'];
-    $macesc=$_GET['mac-esc'];
-    $identity=$_GET['identity'];
-    $bytesinnice=$_GET['bytes-in-nice'];
-    $bytesoutnice=$_GET['bytes-out-nice'];
-    $sessiontimeleft=$_GET['session-time-left'];
-    $uptime=$_GET['uptime'];
-    $refreshtimeout=$_GET['refresh-timeout'];   
-    $linkstatus=$_GET['link-status'];  
+    $mac=$_POST['mac'];
+    $ip=$_POST['ip'];
+    $username=$_POST['username'];
+    $linklogin=$_POST['link-login'];
+    $linkorig=$_POST['link-orig'];
+    $error=$_POST['error'];
+    $trial=$_POST['trial'];
+    $chapid=$_POST['chap-id'];
+    $chapchallenge=$_POST['chap-challenge'];
+    $linkloginonly=$_POST['link-login-only'];
+    $linkorigesc=$_POST['link-orig-esc'];
+    $macesc=$_POST['mac-esc'];
+    $identity=$_POST['identity'];
+    $bytesinnice=$_POST['bytes-in-nice'];
+    $bytesoutnice=$_POST['bytes-out-nice'];
+    $sessiontimeleft=$_POST['session-time-left'];
+    $uptime=$_POST['uptime'];
+    $refreshtimeout=$_POST['refresh-timeout'];   
+    $linkstatus=$_POST['link-status'];  
     ?>
 @endsection
 
